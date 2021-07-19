c88ef45d076aa9534f7b40833b7cee7924860eb2
peng.yu <peng.yu@plaso.me>
2021-07-19 18:42:23 +0800

    theme

---
+++ www/src/constants/meta.ts
@@ -0,0 +1,9 @@
+export const site = {
+  title: `Lennart Jörgens`,
+  titleDefault: `Lennart Jörgens - Software Engineer`,
+  url: `http://how2js.cn`, // Without a trailing slash!
+  dataDomain: `how2js.cn`,
+  description: `Lennart is a software engineer and passionate about working on open source products & building communities around them. He currently works at Gatsby on the open source project.`,
+  image: `/social/default-og-image.png?v=1`,
+  twitter: `@lekoarts_de`,
+}
